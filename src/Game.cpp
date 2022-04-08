4bfb287c3bcafbf4e3a313a58d0acdf0850a28ab
Tobi-D7 <tobid7@outlook.de>
2022-04-08 15:25:00 +0200

    FIX

--- src/Game.cpp
+++ src/Game.cpp
@@ -120,10 +120,10 @@ void Game::Draw(void) const
 }
 void Game::Logic(u32 hDown, u32 hHeld, u32 hUp, touchPosition touch)
 {
-    birdr = (birdv/5)*2;
-    if (birdr >= 0.5)
+    birdr = (birdv/5)*2.5;
+    if (birdr >= 1.5)
     {
-        birdr = 0.5;
+        birdr = 1.5;
     }
     
     if (!tot)
