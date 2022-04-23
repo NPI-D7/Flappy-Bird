d2171dd14582b1ddca50913b411fc1fa714c693d
Tobi-D7 <tobid7@outlook.de>
2022-04-23 08:49:36 +0200

    Add RenderD7 0.7.2

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@ int main()
     RenderD7::Scene::Load(std::make_unique<Game>());
     while (RenderD7::MainLoop())
     {
-        RenderD7::Scene::doDraw();
-        RenderD7::Scene::doLogic(d7_hDown, d7_hHeld, d7_hUp, d7_touch);
+        //RenderD7::Scene::doDraw();
+        //RenderD7::Scene::doLogic(d7_hDown, d7_hHeld, d7_hUp, d7_touch);
         if (d7_hDown & KEY_START)
         {
             RenderD7::ExitApp();
