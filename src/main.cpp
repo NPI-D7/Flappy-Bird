1aabd9e1b005855ec55831bcf2ca8743432fdc22
Tobi-D7 <tobid7@outlook.de>
2023-03-11 15:57:08 +0100

    Custom Logo.lz11 and RenderD7 0.9.2

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,20 @@
 #include "TEX.h"
 
 int main() {
+  rd7_do_splash = true;
   RenderD7::Init::Main("Flappy-Bird");
   RenderD7::Init::NdspFirm();
-
+  RenderD7::DoNpiIntro();
+  RenderD7::FadeOut();
   RenderD7::Scene::Load(std::make_unique<Game>());
   while (RenderD7::MainLoop()) {
     // RenderD7::Scene::doDraw();
     // RenderD7::Scene::doLogic(d7_hDown, d7_hHeld, d7_hUp, d7_touch);
     if (d7_hDown & KEY_START) {
+      RenderD7::FadeIn();
       RenderD7::ExitApp();
     }
+    RenderD7::OnScreen(Top);
     RenderD7::FrameEnd();
   }
   RenderD7::Exit::NdspFirm();
